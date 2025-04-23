2c678c8c56173cf80fd1b2a2c710959df713ebe2
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-23 15:18:07 -0300

    feat: criar DTO para dados de autenticacao do usuario

--- src/main/java/med/voll/api/controller/AutentificacaoController.java
+++ src/main/java/med/voll/api/controller/AutentificacaoController.java
@@ -1,7 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.usuario.DadosAutenticacao;
+import med.voll.api.domain.usuario.DadosAutenticacao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
--- src/main/java/med/voll/api/usuario/DadosAutenticacao.java
+++
@@ -1,2 +0,0 @@
-package med.voll.api.usuario;public record DadosAutenticacao() {
-}
