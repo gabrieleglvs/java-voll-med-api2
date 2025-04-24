ba4a49a861ac0abba4ffa0e0e37c28a0b2f14199
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-24 16:44:41 -0300

    feat: gerar token e retornar apos o login

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login")
-public class AutentificacaoController {
+public class AutenticacaoController {
 
     @Autowired
     private AuthenticationManager manager;
