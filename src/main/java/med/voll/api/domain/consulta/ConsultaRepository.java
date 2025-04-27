adb6bcaa651b6cdc777e2f23604beaef87fbd4e4
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-26 22:29:50 -0300

    feat: criar ConsultaRepository para consultar database

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -1,2 +1,6 @@
-package med.voll.api.domain.consulta;public interface ConsultaRepository {
+package med.voll.api.domain.consulta;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 }
