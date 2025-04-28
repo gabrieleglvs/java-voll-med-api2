d63141a94cb50a3e0b836a8deffd1c5ca8226885
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-28 01:24:56 -0300

    feat: criar consulta no MedicoRepository para verificar se medico esta ativo

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -22,4 +22,11 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
             LIMIT 1
             """)
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
+
+    @Query("""
+            SELECT m.ativo
+            FROM Medico m
+            WHERE m.id = :idMedico
+            """)
+    Boolean findAtivoById(Long idMedico);
 }
