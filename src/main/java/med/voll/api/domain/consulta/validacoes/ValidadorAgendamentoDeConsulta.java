51b652b9503e7be4f0b14dbecd23ac7a8300fb4d
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-28 01:18:02 -0300

    feat: criar interface ValidadorAgendamentoDeConsulta

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorAgendamentoDeConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorAgendamentoDeConsulta.java
@@ -1,2 +1,9 @@
-package med.voll.api.domain.consulta.validacoes;public interface ValidadorAgendamentoDeConsulta {
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+public interface ValidadorAgendamentoDeConsulta {
+
+    void validar(DadosAgendamentoConsulta dados) throws ValidacaoException;
 }
